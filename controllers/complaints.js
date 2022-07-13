fb9dd2a4f9b16057199ba0e9928acdf2950ae208
LiuBaoYing <92360139+LBY89@users.noreply.github.com>
2022-07-13 11:42:27 +0300

    uibuild

--- controllers/complaints.js
+++ controllers/complaints.js
@@ -1,96 +1,105 @@
-const complaintsRouter = require('express').Router()
-const User = require('../models/user')
-const Complaint = require('../models/complaint')
-const jwt = require('jsonwebtoken')
-const multer = require('multer')
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, './uploads/')
-    },
-    filename: (req, file, cb) => {
-        cb(null, new Date().toISOString() + '-' + file.originalname)
-    }
-})
+const complaintsRouter = require("express").Router();
+const User = require("../models/user");
+const Complaint = require("../models/complaint");
+const jwt = require("jsonwebtoken");
+// const multer = require("multer");
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "./uploads/");
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, new Date().toISOString() + "-" + file.originalname);
+//   },
+// });
 
-const upload = multer({ storage: storage })
+// const upload = multer({ storage: storage });
 
-complaintsRouter.get('/:id', (request, response, next) => {
-    Complaint.findById(request.params.id).then(complaint => {
-        if (complaint) {
-            response.json(complaint)
-        } else {
-            response.status(404).end()
-        }
-        
+complaintsRouter.get("/:id", (request, response, next) => {
+  Complaint.findById(request.params.id)
+    .then((complaint) => {
+      if (complaint) {
+        response.json(complaint);
+      } else {
+        response.status(404).end();
+      }
     })
-        .catch(error => next(error))
+    .catch((error) => next(error));
+});
 
-})
+complaintsRouter.get("/", async (request, response) => {
+  const complaints = await Complaint.find({})
+    .populate("user", { firstname: 1, lastname: 1, email: 1 })
+    .populate("comments", { content: 1, complaintId: 1, userId: 1 });
 
-complaintsRouter.get('/', async (request, response) => {
-    const complaints = await Complaint
-        .find({})
-        .populate('user', { firstname: 1, lastname: 1, email: 1 }).populate('comments', {content:1, complaintId: 1, userId: 1})
+  response.json(complaints);
+});
 
-    response.json(complaints)
-  
-})
-
-complaintsRouter.delete('/:id', (request, response, next) => {
-    Complaint.findByIdAndRemove(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
-})
-
-complaintsRouter.put('/:id', async (request, response) => {
-    const { content, title} = request.body
-
-    const updatedComplaint = await Complaint.findByIdAndUpdate(
-        request.params.id, 
-        { content, title },
-        { new: true, runValidators: true, context: 'query' }
-    )
-
-    await updatedComplaint.populate('user', { firstname: 1, lastname: 1, email: 1, id: 1 })
-    
-    response.json(updatedComplaint)
-    
-})
-
-const getTokenFrom = request => {
-    const authorization = request.get('authorization')
-    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-        return authorization.substring(7)
-    }
-    return null
-}
-
-complaintsRouter.post('/', upload.single('image'), async (request, response) => {
-    console.log('request.file', request.file)
-    const body = request.body
-    const token = getTokenFrom(request)
-    const decodedToken = jwt.verify(token, process.env.SECRET)
-    if (!decodedToken.id) {
-        return response.status(401).json({ error: 'token missing or invalid' })
-    }
-    const user = await User.findById(decodedToken.id)
-
-    const complaint = new Complaint( {
-        title: body.title,
-        content: body.content,
-        date: new Date(),
-        user: user._id,
-        image: request.file.path
+complaintsRouter.delete("/:id", (request, response, next) => {
+  Complaint.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
     })
+    .catch((error) => next(error));
+});
 
-    const savedComplaint = await complaint.save()
-    await savedComplaint.populate('user', { firstname: 1, lastname: 1, email: 1, id: 1 })
-    user.complaints = user.complaints.concat(savedComplaint._id)
-    await user.save()
-    
-    response.json(savedComplaint)
-})
+complaintsRouter.put("/:id", async (request, response) => {
+  const { content, title } = request.body;
 
-module.exports = complaintsRouter
+  const updatedComplaint = await Complaint.findByIdAndUpdate(
+    request.params.id,
+    { content, title },
+    { new: true, runValidators: true, context: "query" }
+  );
+
+  await updatedComplaint.populate("user", {
+    firstname: 1,
+    lastname: 1,
+    email: 1,
+    id: 1,
+  });
+
+  response.json(updatedComplaint);
+});
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    return authorization.substring(7);
+  }
+  return null;
+};
+
+complaintsRouter.post("/", async (request, response) => {
+  const body = request.body;
+  console.log("body", body);
+
+  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token missing or invalid" });
+  }
+  const user = await User.findById(decodedToken.id);
+
+  const complaint = new Complaint({
+    title: body.title,
+    content: body.content,
+    date: new Date(),
+    user: user._id,
+    image: body.image,
+  });
+  console.log("n complaint obj", complaint);
+
+  const savedComplaint = await complaint.save();
+  await savedComplaint.populate("user", {
+    firstname: 1,
+    lastname: 1,
+    email: 1,
+    id: 1,
+  });
+  user.complaints = user.complaints.concat(savedComplaint._id);
+  await user.save();
+
+  response.json(savedComplaint);
+});
+
+module.exports = complaintsRouter;
--- controllers/login.js
+++ controllers/login.js
@@ -1,36 +1,37 @@
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
-const loginRouter = require('express').Router()
-const User = require('../models/user')
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const loginRouter = require("express").Router();
+const User = require("../models/user");
 
-loginRouter.post('/', async (request, response) => {
-    const { email, password } = request.body
+loginRouter.post("/", async (request, response) => {
+  const { email, password } = request.body;
 
-    const user = await User.findOne({ email })
-    const passwordCorrect = user === null
-        ? false
-        : await bcrypt.compare(password, user.passwordHash)
+  const user = await User.findOne({ email });
+  const passwordCorrect =
+    user === null ? false : await bcrypt.compare(password, user.passwordHash);
 
-    if (!(user && passwordCorrect)) {
-        return response.status(401).json({
-            error: 'invalid email or password'
-        })
-    }
+  if (!(user && passwordCorrect)) {
+    return response.status(401).json({
+      error: "invalid email or password",
+    });
+  }
 
-    const userForToken = {
-        email: user.email,
-        id: user._id,
-    }
+  const userForToken = {
+    email: user.email,
+    id: user._id,
+  };
 
-    const token = jwt.sign(
-        userForToken, 
-        process.env.SECRET,
-        { expiresIn: 60*60 }
-    )
+  const token = jwt.sign(userForToken, process.env.SECRET, {
+    expiresIn: 600 * 600,
+  });
 
-    response
-        .status(200)
-        .send({ token, id: user._id, email: user.email, firstname: user.firstname, lastname: user.lastname })
-})
+  response.status(200).send({
+    token,
+    id: user._id,
+    email: user.email,
+    firstname: user.firstname,
+    lastname: user.lastname,
+  });
+});
 
-module.exports = loginRouter
+module.exports = loginRouter;
